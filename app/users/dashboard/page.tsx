6d9f860c9dc1a0b80f9f90c2a318d7bfdf639383
rcostarelliTrickgs <renzo@trickgs.com>
2023-06-10 12:19:52 -0300

    wip pages

--- app/users/dashboard/page.tsx
+++ app/users/dashboard/page.tsx
@@ -1,7 +1,6 @@
-import EventCard from "@/app/components/event-card";
-
 export default function Dashboard() {
     return <h1>Holis desde el dashboard</h1>
 }
 
 // pagina con historial de tickets comprados + datos del usuario (nombre, apellido, etc)
+// mostrar nombre de usuario logueado
