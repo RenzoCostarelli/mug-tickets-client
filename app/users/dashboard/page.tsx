b27a78f18237dca52cb5c7f865b4c8efc3f732ba
rcostarelliTrickgs <renzo@trickgs.com>
2023-06-10 12:06:03 -0300

    dashboard folder added

---
+++ app/users/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import EventCard from "@/app/components/event-card";
+
+export default function Dashboard() {
+    return <h1>Holis desde el dashboard</h1>
+}
+
+// pagina con historial de tickets comprados + datos del usuario (nombre, apellido, etc)
