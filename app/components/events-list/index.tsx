24cbf404849da508a9aa8506a33f734d8d005562
rcostarelliTrickgs <renzo@trickgs.com>
2023-08-01 15:27:12 -0300

    apiUrl env created

--- app/components/events-list/index.tsx
+++ app/components/events-list/index.tsx
@@ -3,21 +3,14 @@ import EventCard from "../event-card"
 import s from './events-lists.module.scss'
 
 // all events
-async function getData() {
-  const res = await fetch('https://mug-tickets-server.vercel.app/api/events/');
-  if (!res.ok) {
-    throw new Error('Failed to fetch dataa');
-  } 
-  return res.json();
-}
 
-export default async function EventsList({props}: any) {  
 
-  const { events } = await getData();
+export default function EventsList({props}: any) {
+    console.log('props',props)
     return (
         <div className={s.cardsContainer}>
           {
-          events.map((da: {eventType: string}) => (
+          props.map((da: {eventType: string}) => (
             <EventCard showInfo={da}/>
           ))
           }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        apiUrl: 'https://mug-tickets-server.vercel.app/api',
+    },
     images: {
         domains: [
             'placehold.jp'
