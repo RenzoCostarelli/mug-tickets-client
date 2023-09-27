4ed2ed1f0ea6e5aa262fb7430d4ef2c2fc46d83a
rcostarelliTrickgs <renzo@trickgs.com>
2023-09-27 19:58:39 -0300

    ids added to nextconfig

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
         apiUrl: 'https://mug-tickets-server.vercel.app/api',
         apiKey: '33F85ADC279C7872D63B1B42A1B31',
         MP_PUBLIC_KEY: 'TEST-9bb1f31d-e54b-4a9c-9698-f8f984e21b01',
-        MP_ACCESS_TOKEN: 'TEST-8251247267654899-080408-97c4c8db3fb366628f0449948c58e464-54797482'
+        MP_ACCESS_TOKEN: 'TEST-8251247267654899-080408-97c4c8db3fb366628f0449948c58e464-54797482',
+        GOOGLE_ID: '426944037353-doj012lil9vjrbb60o8gghbrs0khh1sp.apps.googleusercontent.com', 
+        GOOGLE_SECRET: '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 '
         //apiUrl: 'http://localhost:27017/api',
     },
     images: {
