0a65585f3326b77bb453250f202caf9a11ee8b6e
rcostarelliTrickgs <renzo@trickgs.com>
2023-09-28 10:55:36 -0300

    env variables vercel test

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
         MP_ACCESS_TOKEN: 'TEST-8251247267654899-080408-97c4c8db3fb366628f0449948c58e464-54797482',
         GOOGLE_ID: '426944037353-doj012lil9vjrbb60o8gghbrs0khh1sp.apps.googleusercontent.com', 
         GOOGLE_SECRET: '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 '
-        //apiUrl: 'http://localhost:27017/api',
     },
     images: {
         domains: [
