29e53e4105bff20a70c87dbbf4512ccdfac04c25
rcostarelliTrickgs <renzo@trickgs.com>
2023-10-03 16:53:59 -0300

    mp prod env added

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     env: {
         apiUrl: 'https://mug-tickets-server.vercel.app/api',
         apiKey: '33F85ADC279C7872D63B1B42A1B31',
-        MP_PUBLIC_KEY: 'TEST-8c6b35f8-ec70-4f96-a5e3-6266d2da6ea7',
-        MP_ACCESS_TOKEN: 'TEST-5729491792342125-092816-da6d7a81ec2e80006bdef11bd14ef032-1449054997',
+        MP_PUBLIC_KEY: 'APP_USR-61588964-6b13-44ce-bdc2-50541f814921',
+        MP_ACCESS_TOKEN: 'APP_USR-5729491792342125-092816-579911e43169ada6db3696d4c8b9eff7-1449054997',
         GOOGLE_ID: '426944037353-doj012lil9vjrbb60o8gghbrs0khh1sp.apps.googleusercontent.com', 
         GOOGLE_SECRET: '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'
     },
