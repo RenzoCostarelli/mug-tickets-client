b5b8b69e5f2a9685903c6b3e79e759ae183d9a8a
rcostarelliTrickgs <renzo@trickgs.com>
2023-10-04 16:09:03 -0300

    unnused imports removed

--- app/components/nav-bar/index.tsx
+++ app/components/nav-bar/index.tsx
@@ -4,10 +4,7 @@ import Image from 'next/image';
 import s from './nav-bar.module.scss';
 
 import { useSession } from 'next-auth/react';
-import SignOutButton from '../sign-out-button';
-import DashboardButton from '../dashboard-button';
 
-import GoogleButton from '../google-button';
 import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
@@ -34,7 +31,6 @@ export default function Navbar() {
                     <li className={s.link}>
                         <Link href={`/`}>Entradas</Link>
                     </li>
-
                 </ul>
             </div>
         </div>        
