5ef8808a07cb9b1bff30f67f9dda4b7bc3bed7be
Renzo Costarelli <renzo.costarelli@gmail.com>
2023-11-07 14:04:27 -0300

    push test

--- app/components/event-data-list/index.tsx
+++ app/components/event-data-list/index.tsx
@@ -2,11 +2,45 @@ import { EventInfo, Events } from "@/app/types/events";
 import AttendeeList from "../attendee-list";
 import s from './event-data-list.module.scss'
 import { TicketType } from "@/app/types/ticket";
+import { Payment, columns } from "../attendee-list/columns";
+import { DataTable } from "../attendee-list/data-table";
 
 interface EventDataListProps {
     eventData: EventInfo;
   }
 
+  const data: Payment[] = [
+    {
+      id: "m5gr84i9",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
+    },
+    {
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
+    },
+    {
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
+    },
+    {
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
+    },
+    {
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
+    },
+  ]
 export default function EventDataList(props: EventDataListProps) {
   const e = props.eventData.event
   const tickets = e.tickets
@@ -34,7 +68,8 @@ export default function EventDataList(props: EventDataListProps) {
         </div>
       </section>
       <section className={s.attendees_list}>
-        <AttendeeList ticketsList={tickets} />
+        {/* <AttendeeList ticketsList={tickets} /> */}
+        <DataTable columns={columns} data={data}/>
       </section>
     </div>
   );
